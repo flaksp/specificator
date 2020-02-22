bd2cd9597badbc3a8d5900bbc10b1601c7675879
Petr Flaks <petr@flaks.dev>
2020-02-22 15:38:00 +0300

    Fix wrong additionalProperties type

--- src/OpenAPI/Schema/ObjectSchema.ts
+++ src/OpenAPI/Schema/ObjectSchema.ts
@@ -4,7 +4,7 @@ import {Reference} from "../Reference";
 import {Schema, SchemaInterface} from "./Schema";
 
 export interface ObjectSchemaInterface extends SchemaInterface {
-    additionalProperties?: boolean;
+    additionalProperties?: boolean | Schema | Reference;
 
     discriminator?: Discriminator;
 
@@ -31,7 +31,7 @@ export class ObjectSchema extends Schema implements ObjectSchemaInterface, Seria
     /**
      * Value can be boolean or object. Inline or referenced schema MUST be of a [Schema Object](https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#schemaObject) and not a standard JSON Schema. Consistent with JSON Schema, `additionalProperties` defaults to `true`.
      */
-    public additionalProperties?: boolean;
+    public additionalProperties?: boolean | Schema | Reference;
 
     /**
      * Adds support for polymorphism. The discriminator is an object name that is used to differentiate between other schemas which may satisfy the payload description. See [Composition and Inheritance](https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#schemaComposition) for more details.
